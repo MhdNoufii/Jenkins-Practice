f6dbd1f65b9f93a4fbb8efbea9e89710a5e9552f
Noufii <mhdnawffal027@gmail.com>
2025-08-20 07:56:50 +0000

    java file has been added

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Jenkins Docker Pipeline!");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
