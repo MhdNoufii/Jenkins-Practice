c48956fc76d3de2263db165d50cd2b678c29ba6c
Noufii <mhdnawffal027@gmail.com>
2025-08-20 08:06:41 +0000

    Moved File to my Folder

--- helloworld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World from Jenkins Docker Pipeline!");
-        System.out.println("Java Version: " + System.getProperty("java.version"));
-    }
-}
